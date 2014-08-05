1bcff54b6e3f7f0b67a3e99a367c45ecb76d9a04
Springsteen <martin.iv96@gmail.com>
2014-08-05 11:26:34 +0300

    Adding a simple input check to the task.

--- practice_tasks/multiplication.rb
+++ practice_tasks/multiplication.rb
@@ -4,14 +4,17 @@ i = 1
 
 puts "Enter a number between 1 and 3 200 000"
 N = (gets.chomp).to_i
+if N < 0 or N > 3200000
+	puts "You've entered bad index!!! Please try again"
+else
 
-while ( doubles_string.length < N ) 
-	doubles_string += (i*i).to_s 
-	i+=1
+	while ( doubles_string.length < N ) 
+		doubles_string += (i*i).to_s 
+		i+=1
+	end
+
+	puts "The number sequence is: "
+	puts doubles_string
+	puts "The #{N} indexed number in the sequence is: "
+	puts doubles_string[N-1]
 end
-
-puts "The number sequence is: "
-puts doubles_string
-puts "The #{N} number in the sequence is: "
-puts doubles_string[N-1]
-
