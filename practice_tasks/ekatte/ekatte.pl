67691302f8754bf2aa21754c8a6cfcec1fb5032d
Springsteen <martin.iv96@gmail.com>
2014-08-11 16:55:49 +0300

    Sample Database structure ready

--- practice_tasks/ekatte/ekatte.pl
+++ practice_tasks/ekatte/ekatte.pl
@@ -5,6 +5,7 @@ no warnings 'utf8';
 
 sub ParseDoc {
 	$name = @_[0]; 
+	$startRow = @_[1];
 	my $oExcel = new Spreadsheet::ParseExcel;
 
 	my $oBook = $oExcel->Parse($name);
@@ -17,7 +18,7 @@ sub ParseDoc {
 			for(my $iC = $oWkS->{MinCol} ;
 							defined $oWkS->{MaxCol} && $iC <= $oWkS->{MaxCol} ; $iC++) {
 				$oWkC = $oWkS->{Cells}[$iR][$iC];
-				if ( ($iC == 0) && ($iR != 0) ) {
+				if ( ($iC == 0) && ($iR > $startRow) ) {
 					# print "Name => " . $oWkC->Value . "\n" if($oWkC);
 					# push(@nameArray, $oWkC->Value);
 					foreach my $i (@columnsNeeded) {
@@ -57,11 +58,11 @@ our @columnsNeeded;
 my $regionDocName = 'Ek_reg2.xls';
 my $areaDocName = 'Ek_obl.xls';
 my $munDocName = 'Ek_obst.xls';
-
+my $locationDocName = 'Ek_atte.xls';
 Initialize();
 
 @columnsNeeded = (0, 1);
-ParseDoc($regionDocName);
+ParseDoc($regionDocName, 0);
 for (my $e = 0; $e < scalar @nameArray; $e+=2) {
 	my $statementHandler = $dbHandler->prepare("INSERT INTO region
 	                       (name, region_name)
@@ -74,7 +75,7 @@ $dbHandler->commit or die $DBI::errstr;
 undef @nameArray, @columnsNeeded;
 
 @columnsNeeded = (0, 2, 3);
-ParseDoc($areaDocName);
+ParseDoc($areaDocName, 0);
 for (my $e = 0; $e < scalar @nameArray; $e+=3) {
 	my $statementHandler = $dbHandler->prepare("INSERT INTO area
 	                       (name, region, area_abbreviation)
@@ -87,13 +88,26 @@ $dbHandler->commit or die $DBI::errstr;
 undef @nameArray, @columnsNeeded;
 
 @columnsNeeded = (0, 2);
-ParseDoc($munDocName);
+ParseDoc($munDocName, 0);
 for (my $e = 0; $e < scalar @nameArray; $e+=2) {
 	my $statementHandler = $dbHandler->prepare("INSERT INTO municipality
 	                       (name, mun_abbreviation)
 	                        values
 	                       (?, ?)");
 	$statementHandler->execute(@nameArray[$e+1], @nameArray[$e]) or die $DBI::errstr;
+	$statementHandler->finish();
+}
+$dbHandler->commit or die $DBI::errstr;
+undef @nameArray, @columnsNeeded;
+
+@columnsNeeded = (2, 3, 4);
+ParseDoc($locationDocName, 1);
+for (my $e = 0; $e < scalar @nameArray; $e+=3) {
+	my $statementHandler = $dbHandler->prepare("INSERT INTO location
+	                       (name, area, municipality)
+	                        values
+	                       (?, ?, ?)");
+	$statementHandler->execute(@nameArray[$e], @nameArray[$e+1], @nameArray[@e+2]) or die $DBI::errstr;
 	$statementHandler->finish();
 }
 $dbHandler->commit or die $DBI::errstr;
