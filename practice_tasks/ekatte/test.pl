d02934d2742b0d42b080fc78dac02cd00f8b171f
Springsteen <martin.iv96@gmail.com>
2014-08-11 15:53:59 +0300

    Improving some existing code to be reusable and implementing relations between tables.

--- practice_tasks/ekatte/test.pl
+++ practice_tasks/ekatte/test.pl
@@ -3,7 +3,7 @@ use DBI;
 use utf8;
 no warnings 'utf8';
 
-sub ParseRegionDoc {
+sub ParseDoc {
 	$name = @_[0]; 
 	my $oExcel = new Spreadsheet::ParseExcel;
 
@@ -19,47 +19,72 @@ sub ParseRegionDoc {
 				$oWkC = $oWkS->{Cells}[$iR][$iC];
 				if ( ($iC == 0) && ($iR != 0) ) {
 					# print "Name => " . $oWkC->Value . "\n" if($oWkC);
-					push(@nameArray, $oWkC->Value);
-					push(@nameArray, $oWkS->{Cells}[$iR][$iC+1]->Value);
+					# push(@nameArray, $oWkC->Value);
+					foreach my $i (@columnsNeeded) {
+						push(@nameArray, $oWkS->{Cells}[$iR][$i]->Value);
+					}
 				}
 			}
 		}
 	}
 }
 
+
 sub PrepareFilesForParsing {
 	`unzip Ekatte.zip`;
 	`unzip Ekatte_xls.zip`;
+	$dbHandler = DBI->connect("dbi:Pg:dbname=$dbName",
+									$psqlUser,
+									$psqlPassword,
+									{AutoCommit=>0,
+										RaiseError=>1,
+										PrintError=>1
+									}
+								);
 }
 
 sub RemoveUnneededResources {
 	`rm *.xls *.txt Ekatte_xls.zip`;
 }
 
+our $dbHandler;
 our @nameArray;
 our $dbName = 'testdb';
 our $psqlUser = 'martin';
 our $psqlPassword = 'Parola';
-my $dbHandler = DBI->connect("dbi:Pg:dbname=$dbName",
-								$psqlUser,
-								$psqlPassword,
-								{AutoCommit=>0,
-									RaiseError=>1,
-									PrintError=>1
-								}
-							);
-my $docName = 'Ek_reg2.xls';
+our @columnsNeeded;
+my $regionDocName = 'Ek_reg2.xls';
+my $areaDocName = 'Ek_obl.xls';
+
 PrepareFilesForParsing();
-ParseRegionDoc($docName);
+
+@columnsNeeded = (0, 1);
+ParseDoc($regionDocName);
 for (my $e = 0; $e < scalar @nameArray; $e+=2) {
-	my $statementHandler = $dbHandler->prepare("INSERT INTO test_table
-	                       (Name, Region)
+	my $statementHandler = $dbHandler->prepare("INSERT INTO region
+	                       (name, region_name)
 	                        values
 	                       (?, ?)");
-	$statementHandler->execute(@nameArray[$e], @nameArray[$e+1]) or die $DBI::errstr;
+	$statementHandler->execute(@nameArray[$e+1], @nameArray[$e]) or die $DBI::errstr;
 	$statementHandler->finish();
 }
 $dbHandler->commit or die $DBI::errstr;
+undef @nameArray, @columnsNeeded;
+
+
+@columnsNeeded = (0, 2, 3);
+ParseDoc($areaDocName);
+for (my $e = 0; $e < scalar @nameArray; $e+=3) {
+	my $statementHandler = $dbHandler->prepare("INSERT INTO area
+	                       (name, region, area_abbreviation)
+	                        values
+	                       (?, ?, ?)");
+	$statementHandler->execute(@nameArray[$e+1], @nameArray[$e+2], @nameArray[$e]) or die $DBI::errstr;
+	$statementHandler->finish();
+}
+$dbHandler->commit or die $DBI::errstr;
+undef @nameArray, @columnsNeeded;
+
 RemoveUnneededResources();
 
 
