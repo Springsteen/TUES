e33b7f017ee47f18fdb1c95767325c2c76a0a0f5
Springsteen <martin.iv96@gmail.com>
2014-08-25 11:40:52 +0300

    Small js change

--- practice_tasks/myapp/public/javascripts/base.js
+++ practice_tasks/myapp/public/javascripts/base.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
-	$( ".field" ).change(function() {
+	$( ".field" ).keyup(function() {
 		$(this).css({'border-color' : 'red'});
 	});
-	// $(".table-bordered").on("click", ".field", function (){
-	// 		var id = $(this).attr("id");
-	// 		console.log(id + "is clicked on");
-	// 		var v = $(this).text();
-	// 		console.log(v + "is the element text");
-	// 		$(this).html('<input value=' + v + ' name=' + id + '>');
-	// 		$(this).removeClass("field").addClass("active_field");
-	// });
 });
