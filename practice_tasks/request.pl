3e2f0119f710ab5f058cd5927082a5e980242a56
Springsteen <martin.iv96@gmail.com>
2014-09-02 20:36:11 +0300

    smth new

--- practice_tasks/request.pl
+++ practice_tasks/request.pl
@@ -8,25 +8,24 @@ my $password = 'shell_script';
 my $mech = WWW::Mechanize->new();
 
 $mech->get("http://shop.optimalprint.bg/index.php?route=account/login");
-print Dumper($mech->status());
 $mech->form_id('login');
 $mech->field("email", $mail);
 $mech->field("password", $password);
 $mech->submit_form();
 
 $mech->get("http://shop.optimalprint.bg/index.php?route=product/product&path=73_76&product_id=452");
-print Dumper($mech->status());
 $mech->form_id('product');
 $mech->field("quantity",10);
 my $response = $mech->submit_form();
 
 $mech->get("http://shop.optimalprint.bg/index.php?route=checkout/shipping");
-print Dumper($mech->status());
 $mech->form_id("shipping");
-print Dumper($mech->form_id('shipping'));
+print Dumper($mech->field("shipping_method", "econt.econt"));
+print Dumper($mech->find_all_inputs());
+print Dumper($mech->field("shipping_method", "econt.econt"));
 # $mech->set_fields("shipping_method", ["econt.econt", 1]);
-$mech->field( "econt.econt" => 'on' );
-sleep 5;
+print Dumper($mech->set_fields("shipping_method" => "econt.econt"));
+# sleep 5;
 # TODO - FIND A WAY TO TOOGLE THE econt.econt radio button 
 $mech->form_id('econt_form');
 $mech->field("company", "Telebid Pro");
