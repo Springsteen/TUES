f78af41d93e6e550e88ac52c23ed62b0521e9773
Springsteen <martin.iv96@gmail.com>
2014-09-09 10:32:30 +0300

    Trying to use selenium

--- practice_tasks/request.pl
+++ practice_tasks/request.pl
@@ -1,32 +1,53 @@
-use WWW::Mechanize;
+# use WWW::Mechanize;
+# use WWW::Selenium;
 use strict;
 use Data::Dumper;
 my $mail = 'martin.iv96@gmail.com';
 my $password = 'shell_script';
-my $mech = WWW::Mechanize->new();
-$mech->get("http://shop.optimalprint.bg/index.php?route=account/login");
-$mech->form_id('login');
-$mech->field("email", $mail);
-$mech->field("password", $password);
-$mech->submit_form();
-$mech->get("http://shop.optimalprint.bg/index.php?route=product/product&path=73_76&product_id=452");
-$mech->form_id('product');
-$mech->field("quantity",10);
-my $response = $mech->submit_form();
-$mech->get("http://shop.optimalprint.bg/index.php?route=checkout/shipping");
-$mech->form_id("shipping");
-print Dumper($mech->field("shipping_method", "econt.econt"));
-print Dumper($mech->find_all_inputs());
-print Dumper($mech->field("shipping_method", "econt.econt"));
-# $mech->set_fields("shipping_method", ["econt.econt", 1]);
-print Dumper($mech->set_fields("shipping_method" => "econt.econt"));
-# sleep 5;
-# TODO - FIND A WAY TO TOOGLE THE econt.econt radio button
-$mech->form_id('econt_form');
-$mech->field("company", "Telebid Pro");
-$mech->field("postcode", 1000);
-$mech->field("city", "Sofiya");
-$mech->field("quarter", "kv. Strelbishte");
-$mech->field("street", "bul. Gotse Delchev");
-$mech->field("street_num", 103);
-$mech->submit_form();
+
+# my $sel = WWW::Selenium->new(browser_url => "http://shop.optimalprint.bg/index.php?route=account/login");
+# $sel->start;
+# $sel->open("http://shop.optimalprint.bg/index.php?route=account/login");
+# $sel->type("email", $mail);
+# $sel->type("password", $password);
+# $sel->submit("login");
+# print Dumper($sel);
+
+
+
+
+
+
+
+
+
+
+
+# my $mech = WWW::Mechanize->new();
+
+# $mech->get("http://shop.optimalprint.bg/index.php?route=account/login");
+# $mech->form_id('login');
+# $mech->field("email", $mail);
+# $mech->field("password", $password);
+# $mech->submit_form();
+# $mech->get("http://shop.optimalprint.bg/index.php?route=product/product&path=73_76&product_id=452");
+# $mech->form_id('product');
+# $mech->field("quantity",10);
+# my $response = $mech->submit_form();
+# $mech->get("http://shop.optimalprint.bg/index.php?route=checkout/shipping");
+# $mech->form_id("shipping");
+# print Dumper($mech->field("shipping_method", "econt.econt"));
+# print Dumper($mech->find_all_inputs());
+# print Dumper($mech->field("shipping_method", "econt.econt"));
+# # $mech->set_fields("shipping_method", ["econt.econt", 1]);
+# print Dumper($mech->set_fields("shipping_method" => "econt.econt"));
+# # sleep 5;
+# # TODO - FIND A WAY TO TOOGLE THE econt.econt radio button
+# $mech->form_id('econt_form');
+# $mech->field("company", "Telebid Pro");
+# $mech->field("postcode", 1000);
+# $mech->field("city", "Sofiya");
+# $mech->field("quarter", "kv. Strelbishte");
+# $mech->field("street", "bul. Gotse Delchev");
+# $mech->field("street_num", 103);
+# $mech->submit_form();
